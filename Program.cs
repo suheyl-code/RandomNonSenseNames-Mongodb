11077ff310b211843597c17beb65b5dfa15ebd0e
Suheyl Esadli <79663047+suheyl-code@users.noreply.github.com>
2022-05-10 20:50:26 +0300

    .

--- Program.cs
+++ Program.cs
@@ -4,15 +4,18 @@ try
 {
     //DatabaseConnection.SendToMongoDatabase(10);
 
-    //List<PersonModel> myList = DatabaseConnection.RetrieveAll();
-    //var namesBelowTwenty = from items in myList
-    //              where items.Age < 20
-    //              select items;
-    //foreach (var name in namesBelowTwenty)
-    //{
-    //    Console.WriteLine(name);
-    //}
-
+    List<PersonModel> myList = DatabaseConnection.RetrieveAll();
+    var namesBelowTwenty = from items in myList
+                           where items.Age < 20
+                           select items;
+    myList.ForEach(e => Console.WriteLine(e));
+    
+    Console.WriteLine();
+    
+    foreach (var name in namesBelowTwenty)
+    {
+        Console.WriteLine(name);
+    }
 
     //var result = DatabaseConnection.RetrieveById("6279fda9b13eba466568476b");
     //Console.WriteLine(result.ToString());
