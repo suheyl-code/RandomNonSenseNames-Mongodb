8e736ee307e3ab7a0b7fe6dd0fb0ff2bf2479ac3
Suheyl Esadli <79663047+suheyl-code@users.noreply.github.com>
2022-05-10 10:45:00 +0300

    adding custom exception

---
+++ CustomException.cs
@@ -0,0 +1,19 @@
+﻿public class CustomException : Exception
+{
+    private const string defaultMessage = "An Error Occured!";
+
+    public CustomException() : base(defaultMessage)
+    {
+
+    }
+
+    public CustomException(string message) : base(message)
+    {
+        
+    }
+
+    public CustomException(Exception innerException) : base(defaultMessage, innerException)
+    {
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿
-
-// How many first names/last names? set the limit bellow:
 using RandomNames_Mongodb;
+try
+{
+    //DatabaseConnection.SendToMongoDatabase(10);
 
-//DatabaseConnection.SendToMongoDatabase(10);
+    //DatabaseConnection.RetrieveAll();
 
-//DatabaseConnection.RetrieveAll();
-
-var result = DatabaseConnection.RetrieveById("6279fda9b13eba466568476b");
-Console.WriteLine(result.ToString());
-
-
+    var result = DatabaseConnection.RetrieveById("6279fda9b13eba466568476b");
+    Console.WriteLine(result.ToString());
+}
+catch (Exception ex)
+{
+    throw new CustomException(ex);
+}
